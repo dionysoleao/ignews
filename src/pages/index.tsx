ed09641d4f8ce564efc9a0c19a93d49eb77b907c
dionyso <dionysoleao@gmail.com>
2022-01-09 11:01:57 -0500

    head único

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Yeah!</title>
+    </Head>
     <h1 className={styles.title}>
       Hello World
     </h1>
+    </>
   )
 }
